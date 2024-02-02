06b8371f4f16996a1f4eed05bd8418db18189e4d
makevkin <makevkin@mail.ru>
2024-02-02 18:10:20 +0400

    update

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -4,7 +4,9 @@ import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import sun.jvm.hotspot.code.Location;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -24,6 +26,16 @@ public class UserDaoImp implements UserDao {
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+
+   @Override
+   public User getUser(String model, int series) {
+      Query query = sessionFactory.getCurrentSession().createQuery("FROM User user WHERE user.userCar.model = : car_model" +
+              " AND user.userCar.series = : car_series", User.class);
+      query.setParameter("car_model", model);
+      query.setParameter("car_series", series);
+
+      return null;
    }
 
 }
